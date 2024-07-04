55890b607aa1a9e74670fdb9cc2c88abf38311c6
Ilya <gorbov.23@mail.ru>
2024-07-04 13:06:59 +0300

    first commit

---
+++ tests/first.test.ts
@@ -0,0 +1,7 @@
+import test from "@playwright/test";
+
+test.describe("Описание группы теста", async () => {
+    test("Я пишу тест", async () => {
+        console.log("Уиии! Я написал тест!");
+    })
+});
