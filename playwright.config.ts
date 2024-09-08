286f8f8b22ab648b351fb3f26c2191712e5540dc
Ilya <gorbov.23@mail.ru>
2024-09-08 13:22:30 +0300

    thirdt commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,11 +3,20 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   fullyParallel: false,
-  reporter: 'html',
+
   use: {
+    headless: true,
+    browserName: "chromium",
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
     trace: 'off',
   },
-
+  reporter: [["line"], ["json", { outputFile: "test-result.json" }],
+  ['html', {
+    open: "never",
+    outputFolder: "playwright-report/"
+  }]
+],
   projects: [
     {
       name: 'chromium',
--- tests/postCreateUsers.test.ts
+++ tests/postCreateUsers.test.ts
@@ -2,6 +2,13 @@ import { expect, request, test } from "@playwright/test";
 import api from '../api.json';
 import { getRandomEmail, getRandomPhoneNumber } from "../utils/random";
 
+const sportExperience = [
+    "0-6 месяцев",
+    "6-12 месяцев",
+    "1-2 года",
+
+]
+
 test.describe("Тесты на создание клиентов", async () => {
     test("[positive] Создать клиента без опыта", async ({ request }) => {
         const response = await request.post(
@@ -281,5 +288,42 @@ test.describe("Тесты на создание клиентов", async () => {
         );
 
         expect(response.status()).toEqual(200);
+    });
+
+    sportExperience.forEach(experience => {
+        test(`[positive] Создать клиента, без пароля с опытом ${experience}`, async ({ request }) => {
+            const response = await request.post(
+                `${api.urls.base_url_api}${api.paths.users}`,
+                {
+                    headers: {
+                        'Authorization': `${api.tokens.test}`
+                    },
+                    data: {
+                        session_id: "549297f8-e38a-47cd-915e-2a1859102539",
+                        request_id: "4b5b7836-dce6-4b5e-9f18-76be91bd7d37",
+                        request_source: "crm",
+                        data: {
+                            email: getRandomEmail(),
+                            name: "Aotobot",
+                            last_name: "Тестович",
+                            middle_name: "Тестов",
+                            sex: "male",
+                            password: "qwerty1234",
+                            phone: getRandomPhoneNumber(),
+                            birthday: "1990-02-02",
+                            lang: "ru",
+                            user_photo_id: 4,
+                            home_club_id: 1,
+                            club_access: true,
+                            admin_panel_access: false,
+                            class_registration_access: true,
+                            sport_experience: experience
+                        }
+                    }
+                }
+            );
+            
+            expect(response.status()).toEqual(200);
+        });
     });
 });
