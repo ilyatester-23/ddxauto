13555805a6def55db2e2a0729b85691ebfca4a97
Ilya <gorbov.23@mail.ru>
2024-09-14 20:18:43 +0300

    Сделана домашняя работа №4

---
+++ tests/getEmployeePosition.test.ts
@@ -0,0 +1,36 @@
+import { expect, test } from "@playwright/test";
+import api from '../api.json';
+import { getBaseParameters } from "../entities/baseParameters";
+import { log } from "../utils/logger";
+
+const clubIDs = [
+    1, 
+    5, 
+    17
+];
+
+test.describe("API-тесты на получение информации о сотрудниках", async () => {
+    clubIDs.forEach(club_id => {
+        test(`[positive] Получить список сотрудников по позиции и клубам ${club_id}`, async ({ request }) => {
+           const url = `${api.urls.base_url_api}${api.paths.positions}`;
+           const parameters = { ...await getBaseParameters(), position_name: "Персональный тренер", club_id };
+
+           log("Request URL", url);
+           log("Request Parameters", parameters);
+
+           const response = await request.get(
+           url, 
+            {
+                headers: {
+                    'Authorization': `${api.tokens.test}`,
+                },
+                params: parameters,
+            }
+        );
+
+      log("Request Status", response.status());
+      log("Response Body", JSON.stringify(await response.json(), null, '\t'));
+      expect(response.status()).toBe(200);
+    });
+  });
+});
--- tests/postAccessCards.test.ts
+++ tests/postAccessCards.test.ts
@@ -1,23 +1,17 @@
-import { expect, request, test } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 import api from '../api.json';
 import { getRandomPhoneNumber } from "../utils/random";
 import { log } from "../utils/logger";
 
 test.describe("Тесты на создание карт/браслетов доступа", async () => {
-    test("[positive] Создать пользователю карту", async ({ request }) => {
-        const response = await request.post(
-            `${api.urls.base_url_api}${api.paths.access_cards}`,
-            {
-                headers: {
-                    'Authorization': `${api.tokens.test}`
-                },
-                data: {
+    test.only("[positive] Создать пользователю карту", async ({ request }) => {
+        const url = `${api.urls.base_url_api}${api.paths.access_cards}`;
+        const requestBody = {
                     session_id: "549297f8-e38a-47cd-915e-2a1859102539",
                     request_id: "4b5b7836-dce6-4b5e-9f18-76be91bd7d37",
-                    request_source: "string",
+                    request_source: "crm",
                     data: [
-                      {
-                        id: 0,
+                    {
                         access_card_number: getRandomPhoneNumber(),
                         user_id: 1746590,
                         type: "disposable_card",
@@ -27,15 +21,23 @@ test.describe("Тесты на создание карт/браслетов до
                         block_previous_card: true,
                         payable: "guest",
                         blocked_at: "2024-10-25T07:27:23Z"
-                      }
-                    ]
-                }
-            }
-        );
+                    }
+                ]
+        };
         
-        log("request url", URL);
+        log("Request body:", JSON.stringify(requestBody, null, '\t'));
+
+        const response = await request.post(
+            url,
+            {
+                headers: {
+                    'Authorization': `${api.tokens.test}`
+                },
+                data: requestBody
+            });
+        
+        log("request url", url);
         log("request status", response.status());
-        log("request body", JSON.stringify(await request.json, null, '\t'));
         log("response body", JSON.stringify(await response.json(), null, '\t'));
         expect(response.status()).toEqual(200)
     })
