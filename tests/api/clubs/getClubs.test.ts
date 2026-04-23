2ec61dff747cc076c1142f479c79556da2c49570
i.gorbov@ddxfitness.ru <i.gorbov@ddxfitness.ru>
2026-04-23 10:43:05 +0300

    12 урок правки

--- tests/api/clubs/getClubs.test.ts
+++ tests/api/clubs/getClubs.test.ts
@@ -4,15 +4,7 @@ import { getBaseParameters } from "../../../entities/baseParameters";
 
 test.describe("API-тесты на получение списка клубов", async () => {
     test("[positive] получить список клубов", async ({ request }) => {
-        const response = await request.get(
-            `${api.urls.base_url_api}${api.paths.clubs}`,
-            {
-                headers: {
-                    'Authorization': `${api.tokens.test}`
-                },
-                params: {...await getBaseParameters()}
-            }
-        );
+        
 
         expect(response.status()).toEqual(200);
     });
